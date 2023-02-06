f16f67a4b35b6e421a654d8773698165114efd77
Haseebm01 <haseebdr01@gmail.com>
2023-02-05 23:41:25 -0500

    Added & modified

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express(); 
 const routeController = require('./controllers/routes');
 
-
+app.use(express.json());
 
 app.use('/routes', routeController);
