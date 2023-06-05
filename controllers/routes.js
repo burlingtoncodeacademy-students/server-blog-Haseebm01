6fef5974adaea34a807a40a89dcadb68f6ed3a86
Haseebm01 <haseebdr01@gmail.com>
2023-06-05 19:23:42 -0400

    Added & Modified

--- controllers/routes.js
+++ controllers/routes.js
@@ -20,9 +20,23 @@
 
     // Get one by ID
     router.get('/:post_id', (req, res) => {
-        try {
-            const post_id = req.params.post_id;
 
+        try {
+
+            // let foundID = false;
+            // let result;
+            // const post_id = req.params.post_id;
+            // // console.log(post_id);
+
+            // blog.every(record => {
+            //     if(record.post_id == post_id) {
+            //         result = record;
+            //         foundID = true;
+            //     } 
+
+            //     return foundID;
+            // })
+            const post_id = req.params.post_id;
             let result = blog.filter(i => i.post_id == post_id);
 
             if(result.length > 0) {
@@ -35,7 +49,18 @@
                     message: `Record not found.`
                 })
             }
-        }catch (err) {
+
+            // foundID ?
+
+            // res.status(200).json({
+            //     status: `Found item at ID: ${post_id}`,
+            //     result
+            // }) :
+            // res.status(404).json({
+            //     message: `Record not found`
+            // })
+
+        } catch (err) {
             res.status(500).json({
                 ERROR: err.message
             })
@@ -109,9 +134,82 @@
         }
     });
 
+    // Put one by ID - Update an existing entery
+    router.put('/:post_id', (req, res) => {
+        
+        try {
 
+            //1. Grab ID value
+            const post_id = Number(req.params.post_id);
 
+            //2. Consider what is the new data within the body
+            const { title, author, body } = req.body;
 
+            //3. Detail those updates (in an obj)
+            const updatedObj = {
+                post_id, title, author, body
+            }
+
+            //4. Locate our Blog
+            fs.readFile(fsPath, (err, data) => {
+                if(err) throw err;
+
+                const database = JSON.parse(data);
+
+            //5. Locate our document & change it
+            database.forEach((obj, i) => {
+                if(obj.post_id === post_id) {
+                    database[i] = updatedObj;
+                }
+            })
+
+            //6. Update our Blog
+            fs.writeFile(fsPath, JSON.stringify(database), err => console.log(err));
+            })
+
+            res.status(200).json({
+                message: `Entry updated!`
+            })
+
+        } catch (err) {
+            res.status(500).json({
+                error: err.message
+            })
+        }
+    });
+
+    // Delete one by ID
+    router.delete('/post_id', (req, res) => {
+
+        try {
+
+            //1. Find out what ID we need 
+            const post_id = Number(req.params.post_id);
+
+            //2. Locate our database
+            fs.readFile(fsPath, (err, data) => {
+                if(err) throw err;
+
+                const blog = JSON.parse(data);
+
+            //3. Locate our document & Remove it
+            const keptDocuments = blog.filter(i => i.post_id !== post_id)
+            // console.log('Kept: ', keptDocuments); 
+
+            //4. Update blog with current list
+            fs.writeFile(fsPath, JSON.stringify(keptDocuments), err => console.log(err));
+            });
+
+            //5. Respond to the client
+            res.status(200).json({
+                message: `Entry has been deleted`
+            })
+        } catch (err) {
+            res.status(500).json({
+                error: err.message
+            })
+        }
+    });
 
 
 
